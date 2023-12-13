0d97956847bee7faaa5fd9c49eb94bdea8611029
DiegoLudena <d_l_m88@yahoo.es>
2023-12-13 12:17:28 +0100

    Fin del ejercicio

--- src/tarea1/TemperaturaMaxima.java
+++ src/tarea1/TemperaturaMaxima.java
@@ -10,24 +10,19 @@ public class TemperaturaMaxima {
 		int[] temperaturas = new int[3650];
 		//Creo el bucle para rellenarla de forma aleatoria
 		for(int i=0;i<temperaturas.length;i++) {
+						
+		//Creo el objeto Random
+		Random temperaturaaleatoria= new Random();
 			
-			
-			//Creo el objeto Random
-			Random temperaturaaleatoria= new Random();
-			
-			/*Como nextInt(int) crea un numero entre 0 y uno menos que el int, pongo 81 de limite
-				y luego resto 30 para que el numero esté entre -30 y 50*/
-			temperaturas[i]=(temperaturaaleatoria.nextInt(81))-30;
-			//System.out.println(temperaturas[i]);
+		/*Como nextInt(int) crea un numero entre 0 y uno menos que el int, pongo 81 de limite
+			y luego resto 30 para que el numero esté entre -30 y 50*/
+		temperaturas[i]=(temperaturaaleatoria.nextInt(81))-30;
 		}
 		
-
 		int longitud = (temperaturas.length)/10;
 		int comienzo = 0;
 
-		
 		//Creo una matrix de 10 hilos
-		
 		BuscaMaxima arrayBuscaMaxima[] = new BuscaMaxima[10];
 		int[] maxima = new int[10];
 		
@@ -53,13 +48,9 @@ public class TemperaturaMaxima {
 			}
 		    
 		    //Devuelvo el resultado maximo
-		    maxima[i] = arrayBuscaMaxima[i].getMaxima();
-			
-			
+		    maxima[i] = arrayBuscaMaxima[i].getMaxima();				
 		}
-		
-	
-		
+					
 		//comparo las 4 temperaturas máximas: las pongo en un array y repito el bucle de buscamaxima
 		int comparacion=maxima[0];
 		for(int i=0;i<maxima.length-1;i++) {
@@ -67,7 +58,6 @@ public class TemperaturaMaxima {
 			if(comparacion<maxima[i]){
 				comparacion=maxima[i];
 			}
-
 	}
 		System.out.println("La temperatura máxima de de " + comparacion + "ºC");
 }
